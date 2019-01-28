95638a53c68bcd6a3c7c6faacaf135264284edb4
Stephan Fischer <oktopus.werbung@live.de>
2019-01-28 20:39:11 +0100

    slyer eingebaut

---
+++ js/flexslider.js
@@ -0,0 +1,14 @@
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider();
+  });
+</script>
+
+
+	<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+    controlNav: "thumbnails"
+    });
+  });
+    </script>
